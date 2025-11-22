ed10b13731805eb3bbd6b4d3111c93874b88a6cf
Samer526 <sanerdahi.100@gmail.com>
2025-11-23 01:29:38 +0400

    upload birthday website

--- effect.js
+++ effect.js
@@ -173,22 +173,22 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
-			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
-				});
-				
-			}
-			else{
-				msgLoop(i);
-			}			
+  $("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
+    i=i+1;
+    $("p:nth-child("+i+")").fadeIn('slow').delay(1000);
+    if(i==50){
+      $("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+        $('.cake').fadeIn('fast');
+        // إظهار الصورة بعد انتهاء الرسائل
+        $("#final-photo").fadeIn(2000);
+      });
+    }
+    else{
+      msgLoop(i);
+    }           
+  });
+}
 
-		});
-			// body...
-		}
 		
 		msgLoop(0);
 		
